ebca1d97329ae26d16ab00bbee81bcb7af5c1366
joey <joeytsai@gmail.com>
2018-05-07 22:19:59 -0700

    Did I not delete this shiz

--- src/main/kotlin/rando/techiedelight.quora.kt
+++ src/main/kotlin/rando/techiedelight.quora.kt
@@ -3,6 +3,15 @@ package rando
 // https://techiedelight.quora.com/500-Data-Structures-and-Algorithms-practice-problems-and-their-solutions
 
 fun `find pair with given sum in the array`(array: IntArray, sum: Int): Pair<Int, Int> {
-
-    return Pair(1, 1)
+    for (i in array.indices) {
+        val first = array[i]
+        for (j in i..array.lastIndex) {
+            val second = array[j]
+            val s = first + second
+            if (s == sum) {
+                return Pair(first, second)
+            }
+        }
+    }
+    throw RuntimeException("no pairs for sum=$sum in array=$array")
 }
--- src/test/kotlin/rando/Techiedelight_quoraKtTest.kt
+++ src/test/kotlin/rando/Techiedelight_quoraKtTest.kt
@@ -1,12 +1,15 @@
 package rando
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 
 class Techiedelight_quoraKtTest {
     @Test
     fun testArraySum() {
-        println("sup")
+        val array = intArrayOf(1, 2, 3, 4)
+        val pair24 = `find pair with given sum in the array`(array, 6)
+        assertEquals(Pair(2, 4), pair24)
 
     }
 }
